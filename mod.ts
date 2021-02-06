34a0b2d6d59e773f998e083cd78f5126da4e8351
ryosukehagiwara <hukurahagi1109@gmail.com>
2021-02-06 23:02:48 +0900

    add std logger

--- mod.ts
+++ mod.ts
@@ -1,7 +1,31 @@
+// @see "https://deno.land/std@0.86.0/log"
+import * as log from 'https://deno.land/std/log/mod.ts';
+
+// logger setting
+await log.setup({
+  handlers: {
+    console: new log.handlers.ConsoleHandler("DEBUG"),
+  },
+  loggers: {
+    default: {
+      level: "WARNING",
+      handlers: ["console"],
+    },
+  },
+});
+
+// @see "https://github.com/r-spacex/SpaceX-API"
 async function downloadLaunchData(){
+  log.info("Downloading launch data...")
   const response = await fetch("https://api.spacexdata.com/v4/launches/latest",{
     method:"GET"
   })
+
+  if(!response.ok){
+    log.warning("Problem downloading launch data.")
+    throw new Error("Launch data download failed.")
+  }
+
   const data = await response.json()
   console.log(data)
 }
